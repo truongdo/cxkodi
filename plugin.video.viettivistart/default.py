f7af7de4c03fcc514527838a37db50f82d86e904
vdvinh <vdvinh@gmail.com>
2015-05-03 10:33:45 +0700

    Init

--- plugin.video.viettivistart/default.py
+++ plugin.video.viettivistart/default.py
@@ -1,2 +1,7 @@
 import os, xbmc
-xbmc.Player().play('http://s3.amazonaws.com/KA-youtube-converted/ANyVpMS3HL4.mp4/ANyVpMS3HL4.mp4')
+from viettivi import addon as viettv
+
+cns = viettv.getChannels()
+for cn in cns:
+	if cn['label'] == 'VTV3 HD':
+		xbmc.Player().play('http://s3.amazonaws.com/KA-youtube-converted/ANyVpMS3HL4.mp4/ANyVpMS3HL4.mp4')
