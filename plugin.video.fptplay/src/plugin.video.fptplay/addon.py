9154f050bed70d23dd39d5c15ec5ecbf32c5f6fa
vdvinh <vdvinh@gmail.com>
2015-06-07 15:51:19 +0700

    re org add support hot event

--- plugin.video.fptplay/src/plugin.video.fptplay/addon.py
+++ plugin.video.fptplay/src/plugin.video.fptplay/addon.py
@@ -5,6 +5,7 @@ from xbmcswift2 import Plugin,xbmcaddon, xbmc
 import urlfetch
 from BeautifulSoup import BeautifulSoup
 import json
+import re
 
 plugin = Plugin()
 __settings__ = xbmcaddon.Addon(id='plugin.video.fptplay')
@@ -12,9 +13,48 @@ crawurl = 'http://fptplay.net/livetv'
 
 def getAllChannels():
     cns = []
+    cns.extend(getEvents(crawurl))
     cns.extend(getChannels(crawurl))
     return cns
 
+def getEvents(url):
+    cns = []
+    result = None
+    result = urlfetch.fetch(
+        url,
+        headers={
+            'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36'
+            })
+    if result.status_code != 200 :
+        plugin.log.error('Something wrong when get list fpt play event !')
+        return None
+    soup = BeautifulSoup(result.content, convertEntities=BeautifulSoup.HTML_ENTITIES)
+    
+    item = soup.find('ul', {'class' : 'slider_event'})
+    if item == None :
+        return None
+    itemlinks = item.findAll('a')
+    if itemlinks == None :
+        return None
+    for item in itemlinks:
+        title = item.get('title')
+        link = item.get('href')
+        img = item.find('img')
+        imgthumbnail = ''
+        if img != None :
+            imgthumbnail = img.get('src')
+        if not imgthumbnail :
+            continue
+        cn = {
+                'label': title,
+                'path': plugin.url_for('plays', id = link),
+                'thumbnail':imgthumbnail,
+                'is_playable': True
+            }
+        if cn not in cns :
+            cns.append(cn)
+    return cns
+    
 def getChannels(url):
     cns = []
     result = None
@@ -75,8 +115,11 @@ def getChannels(url):
 
 def getLink(id = None):
     
-    plugin.log.info('Get Link For Id ' + id)
-    
+    if id.startswith('http://') :
+        #is event
+        id = getChannelIdFromEventLink(id)
+    if id == None :
+        return None
     result = urlfetch.post(
         'http://fptplay.net/show/getlinklivetv',
         data={"id": id,
@@ -92,8 +135,29 @@ def getLink(id = None):
         plugin.log.error("Can't get link for id " + id)
         return None
     info = json.loads(result.content)
-    print info
     return info['stream']
+    
+def startChannel():
+    channelid = __settings__.getSetting('start_channelid')
+    link = getLink(channelid)
+    xbmc.Player().play(link)
+
+def getChannelIdFromEventLink(url = None) :
+    if url == None :
+        return None
+    result = None
+    result = urlfetch.fetch(
+        url,
+        headers={
+            'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36'
+            })
+    if result.status_code != 200 :
+        plugin.log.error('Something wrong when get content of event link !')
+        return None
+    m = re.search(r"var id = '([^']+)';",result.content)
+    if m == None :
+        return None
+    return m.group(1)
 
 @plugin.route('/')
 def index():
@@ -104,11 +168,6 @@ def index():
 def plays(id):
     link = getLink(id)
     plugin.set_resolved_url(link)
-
-def startChannel():
-    channelid = __settings__.getSetting('start_channelid')
-    link = getLink(channelid)
-    xbmc.Player().play(link)
     
 if __name__ == '__main__':
     plugin.run()
