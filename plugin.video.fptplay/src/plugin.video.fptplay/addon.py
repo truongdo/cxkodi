01ac08cd9c3e063881bb2ccaef31e582e2fbbe94
vdvinh <vdvinh@gmail.com>
2015-09-09 20:42:33 +0700

    Fix Bug

--- plugin.video.fptplay/src/plugin.video.fptplay/addon.py
+++ plugin.video.fptplay/src/plugin.video.fptplay/addon.py
@@ -128,7 +128,8 @@ def getLink(id = None):
             },
         headers={'Content-Type': 'application/x-www-form-urlencoded',
                 'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36',
-                'X-Requested-With':'XMLHttpRequest'
+                'X-Requested-With':'XMLHttpRequest',
+                'Referer':'http://fptplay.net/livetv'
                 }
         )
 
